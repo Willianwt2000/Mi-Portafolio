e98ca3d12e410be255e782cdbd54daba27725843
willianwt2000 <willianmetelien@gmail.com>
2023-07-14 03:56:01 -0400

    modificando las secciones y la seccion contacto

--- validacion.js
+++ validacion.js
@@ -1,1 +1,20 @@
-//Haz tú validación en javascript acá
+/*Funcion para scrolear la parte del heather */
+
+window.addEventListener('scroll', function() {
+  let menuSection = document.querySelector('.menu');
+  let scrollPosition = window.scrollY;
+
+  if (scrollPosition === 0) {
+    menuSection.classList.remove('transparent');
+  } else {
+    menuSection.classList.add('transparent');
+  }
+});
+
+
+// Validacion de input
+
+function enviandoFormulario(params) {
+  console.log('Enviando formulario')
+  return false
+}
