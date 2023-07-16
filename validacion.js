04c9e10b426eb4c9bd69508c6366ae5e6392d9a2
willianwt2000 <willianmetelien@gmail.com>
2023-07-16 15:57:36 -0400

    estilando portafolios/ en proceso

--- validacion.js
+++ validacion.js
@@ -1,20 +1,20 @@
-/*Funcion para scrolear la parte del heather */
+// /*Funcion para scrolear la parte del heather */
 
-window.addEventListener('scroll', function() {
-  let menuSection = document.querySelector('.menu');
-  let scrollPosition = window.scrollY;
+// window.addEventListener('scroll', function() {
+//   let menuSection = document.querySelector('.menu');
+//   let scrollPosition = window.scrollY;
 
-  if (scrollPosition === 0) {
-    menuSection.classList.remove('transparent');
-  } else {
-    menuSection.classList.add('transparent');
-  }
-});
+//   if (scrollPosition === 0) {
+//     menuSection.classList.remove('transparent');
+//   } else {
+//     menuSection.classList.add('transparent');
+//   }
+// });
 
 
-// Validacion de input
+// // Validacion de input
 
-function enviandoFormulario(params) {
-  console.log('Enviando formulario')
-  return false
-}
+// function enviandoFormulario(params) {
+//   console.log('Enviando formulario')
+//   return false
+// }
